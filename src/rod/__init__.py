fc5d5efa669101537d16e8a708ac87cc8d4e0e9c
Diego Ferigo <diego.ferigo@iit.it>
2022-10-19 11:38:43 +0200

    Automatically select default verbosity by checking editable installation

--- src/rod/__init__.py
+++ src/rod/__init__.py
@@ -21,3 +21,31 @@ from .scene import Scene
 from .sdf import Sdf
 from .visual import Visual
 from .world import World
+
+
+def _is_editable():
+
+    import importlib.util
+    import pathlib
+    import site
+
+    # Get the ModuleSpec of rod
+    rod_spec = importlib.util.find_spec(name="rod")
+
+    # This can be None. If it's None, assume non-editable installation.
+    if rod_spec.origin is None:
+        return False
+
+    # Get the folder containing the rod package
+    rod_package_dir = str(pathlib.Path(rod_spec.origin).parent.parent)
+
+    # The installation is editable if the package dir is not in any {site|dist}-packages
+    return rod_package_dir not in site.getsitepackages()
+
+
+# Initialize the logging verbosity
+logging.configure(
+    level=logging.LoggingLevel.DEBUG if _is_editable() else logging.LoggingLevel.WARNING
+)
+
+del _is_editable
