c6ccc6095dfcbc3e23425f1e0419e575b5f22637
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2024-04-26 10:37:22 +0200

    Cache gazebo executable path

--- src/rod/utils/gazebo.py
+++ src/rod/utils/gazebo.py
@@ -7,8 +7,13 @@ from typing import Union
 
 
 class GazeboHelper:
-    @staticmethod
-    def get_gazebo_executable() -> pathlib.Path:
+    _cached_executable: pathlib.Path = None
+
+    @classmethod
+    def get_gazebo_executable(cls) -> pathlib.Path:
+        if cls._cached_executable is not None:
+            return cls._cached_executable
+
         gz = shutil.which("gz")
         ign = shutil.which("ign")
 
@@ -25,13 +30,17 @@ class GazeboHelper:
             raise TypeError(executable)
 
         # Check if the sdf plugin of the simulator is installed
-        cp = subprocess.run([executable, "sdf", "--help"], capture_output=True)
-
-        if cp.returncode != 0:
+        try:
+            subprocess.run(
+                [executable, "sdf", "--help"], check=True, capture_output=True
+            )
+        except subprocess.CalledProcessError as e:
             msg = f"Failed to find 'sdf' command part of {executable} installation"
-            raise RuntimeError(msg)
+            raise RuntimeError(msg) from e
 
-        return executable
+        cls._cached_executable = executable
+
+        return cls._cached_executable
 
     @staticmethod
     def has_gazebo() -> bool:
@@ -94,17 +103,19 @@ class GazeboHelper:
                 fp.write(model_description_string)
                 fp.close()
 
-            cp = subprocess.run(
-                [str(gazebo_executable), "sdf", "-p", fp.name],
-                text=True,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT,
-                check=True,
-            )
-
-        if cp.returncode != 0:
-            print(cp.stdout)
-            raise RuntimeError("Failed to process the input with sdformat")
+            try:
+                cp = subprocess.run(
+                    [str(gazebo_executable), "sdf", "-p", fp.name],
+                    text=True,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.STDOUT,
+                )
+            except subprocess.CalledProcessError as e:
+                if cp.returncode != 0:
+                    print(cp.stdout)
+                    raise RuntimeError(
+                        "Failed to process the input with sdformat"
+                    ) from e
 
         # Get the resulting SDF string
         sdf_string = cp.stdout
